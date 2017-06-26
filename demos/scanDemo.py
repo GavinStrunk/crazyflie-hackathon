dcef98b320e8c0b3e136a36263e4bb5c90b56643
Gavin Strunk <GStrunk@slb.com>
2017-06-26 17:29:47 -0400

    the demo is now able to scan for the drone, open the link, and change the led color after you open the client gui and set the led tab setting. I haven't figure out how to make that work yet.

--- demos/scanDemo.py
+++ demos/scanDemo.py
@@ -1,4 +1,23 @@
 import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.mem import MemoryElement
+
+uri = "radio://0/80/250k"
+
+def connectcf(self):
+
+	mem = cf.mem.get_mems(MemoryElement.TYPE_DRIVER_LED)
+	print(mem[0].leds[0])
+	if(len(mem) > 0):
+		print("try to change led")
+		mem[0].leds[1].set(0,255,0)
+		mem[0].leds[2].set(0,0,255)
+		mem[0].write_data(None)
+	else:
+		print("nothing found")
+
+	cf.close_link()
+
 
 cflib.crtp.init_drivers(enable_debug_driver=False)
 
@@ -7,3 +26,13 @@ available = cflib.crtp.scan_interfaces()
 print("Crazyflies found:")
 for i in available:
 	print(i[0])
+
+print("Create crazyflie object")
+
+cf = Crazyflie()
+
+cf.connected.add_callback(connectcf)
+print("open link")
+cf.open_link(available[0][0])
+
+
